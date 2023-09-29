e6e54df7c2c41f3c536e0bf9150cee9bd6b3c410
Harry Greentree <harrygreentree@icloud.com>
2023-09-29 15:03:41 +0100

    Question 3b: Added an even faster sender

--- select/select.go
+++ select/select.go
@@ -21,19 +21,35 @@ func fastSender(c chan<- int) {
 	}
 }
 
+func fasterSender(c chan<- []int) {
+	slice := []int{1, 2, 3}
+	for {
+		time.Sleep(200 * time.Millisecond)
+		c <- slice
+	}
+}
+
 // main starts the two senders and then goes into an infinite loop of receiving their messages.
 func main() {
+	slices := make(chan []int)
+	go fasterSender(slices)
 	ints := make(chan int)
 	go fastSender(ints)
 	strings := make(chan string)
 	go slowSender(strings)
 
 	for { // = while(true)
+		// We select which print to chose based on which channel has a value
+		// The slower sender takes longer to populate the channel
+		// the faster sender takes less time
+		// so we have more int messages than string messages
 		select {
 		case s := <-strings:
 			fmt.Println("Received a string", s)
 		case i := <-ints:
 			fmt.Println("Received an int", i)
+		case xs := <-slices:
+			fmt.Println("Received a slice", xs)
 		}
 	}
 }
